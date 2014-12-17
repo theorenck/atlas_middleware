f3c700db0b60ed6a71e35a931c45b8c47ceb2bf0
Theo Renck <carlosrenck@gmail.com>
2014-12-17 10:33:25 -0200

    run.bat reescrito para rodar cluster de 6 instancias com a opção c

--- run.bat
+++ run.bat
@@ -1,1 +1,10 @@
-rails s -e production -p 3000
+@ECHO OFF
+
+SET CLUSTERED=%1
+IF NOT DEFINED CLUSTERED SET CLUSTERED=n
+
+IF %CLUSTERED% == c (
+  ruby cluster_start.rb
+) ELSE (
+  thin start -e production -a 127.0.0.1 -p 3000 
+) 
